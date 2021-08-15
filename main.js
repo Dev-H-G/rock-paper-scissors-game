2bcc5a7046330504ccea1c68127ed60614db2b57
Hossein Ghahremani <Dev.h.ghahremani@gmail.com>
2021-08-15 18:29:02 +0430

    add codes to find winner

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ if (userchoice) {
 const computerchoices = Math.floor(Math.random() * 3);
 const computerchoice = choices[computerchoices];
 alert(`computer choice is :${computerchoice}`);
+if (userchoice === computerchoice) {
+    alert("No one wins!");
+} else if (userchoice === "rock") {
+    if (computerchoice === "scissors") {
+        alert("You are the winner!");
+    } else {
+        alert("You are the loser!");
+    }
+} else if (userchoice === "paper") {
+    if (computerchoice === "scissors") {
+        alert("You are the winner!");
+    } else {
+        alert("You are the loser!");
+    }
+} else if (userchoice === "scissors") {
+    if (computerchoice === "paper") {
+        alert("You are the winner!");
+    } else if (computerchoice === "rock") {
+        alert("You are the loser!");
+    }
+}
