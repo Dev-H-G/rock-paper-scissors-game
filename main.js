7cfa42acdaeed31ce1a7af8c124ecfcbc160fa48
Hossein Ghahremani <Dev.h.ghahremani@gmail.com>
2021-08-15 18:25:46 +0430

    add code to receive user choice

---
+++ main.js
@@ -0,0 +1,9 @@
+const choices = ["rock", "paper", "scissors"];
+
+const userchoice = prompt(" choose rock , paper , scissors:");
+if (userchoice) {
+    alert(`You choose : ${userchoice}`);
+} else {
+    alert("You cheated!");
+
+}
